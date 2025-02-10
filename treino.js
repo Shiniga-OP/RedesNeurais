b31631e6677b479d223acbb47b2d02aea086cf39
Shiniga-OP <shinigadev@gmail.com>
2025-02-10 20:06:50 -0300

    Create treino.js

---
+++ treino.js
@@ -0,0 +1,15 @@
+const treino =
+  [
+    { entrada: "olá", saida: "oi" },
+    { entrada: "tudo bem?", saida: "tudo" },
+    { entrada: "oi", saida: "olá" },
+    { entrada: "eae", saida: "eai" },
+    { entrada: "eai", saida: "saudações" },
+    { entrada: "saudações", saida: "eae" }
+  ];
+
+const vocabC =
+  // saudações:
+  "oi olá saudação saudações eae eai "+
+  // expressões:
+  "verdade verdadeiro falso tudo bem mal bom ok é sim não melhor pior mais e você está se enquanto continua continuar continue pode ser";
